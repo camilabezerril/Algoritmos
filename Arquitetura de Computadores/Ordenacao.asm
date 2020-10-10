9f7cd829b5e2f815a8c574be73733785fc6a8186
Camila Bezerril <camilabezerril@outlook.com>
2020-10-10 13:04:18 -0300

    Update Ordenacao.asm

--- Arquitetura de Computadores/Ordenacao.asm
+++ Arquitetura de Computadores/Ordenacao.asm
@@ -1,102 +1,81 @@
 .data
-
 file: .asciiz "teste.txt"      # filename for input
 buffer: .space 512            # 512 / 4 = 128 space for words
-elem: .space 50
-array: .space 512
+
+.align 4
+numbers: .space 512
 
 # Mensagens
-vetorMsg: .asciiz "O vetor a ser ordenado é: "
-numElemMsg: .asciiz "O numero de elementos é: "
 newline: .asciiz "\n"
 
 .text
-
-###################### LEITURA DO ARQUIVO ######################
+####################### LEITURA DO ARQUIVO ########################
 AbrirArquivo:
-	li	$v0, 13          # system call for open file
-	la	$a0, file        # input file name
-	li	$a1, 0           # flag for read-only
-	li	$a2, 0           # mode is ignored
-	syscall			 # open a file 
-	move	$s0, $v0         # save the file descriptor 
+	li	$v0, 13          		# system call for open file
+	la	$a0, file        		# input file name
+	li	$a1, 0           		# flag for read-only
+	li	$a2, 0           		# mode is ignored
+	syscall			 		# open a file 
+	move	$s0, $v0         		# save the file descriptor 
 	
 LerArquivo:
-	li	$v0, 14			# 14 = read from file
-	move 	$a0, $s0      		# file descriptor 
-	la	$a1, buffer		# buffer to hold int charged in a1
-	li	$a2, 512		# Read 512 bytes - size of buffer
-	syscall
-
-	li	$v0, 4			# 1 = print int // but txt is a string = 4	
-	la	$a0, vetorMsg
-	syscall
-	la	$a0, buffer
-	syscall				# print int
-	la	$a0, newline
+	li	$v0, 14				# 14 = read from file
+	move 	$a0, $s0      			# file descriptor 
+	la	$a1, buffer			# buffer to hold int charged in a1
+	li	$a2, 512			# Read 512 bytes - size of buffer
 	syscall
 	
-############## CONTA ELEMENTOS E USA PROCEDIMENTO PARA CONVERTER STRING PARA INTEIRO - armazenando em array ##############
-j LerBuffer
-
-PrintaElementos:
-	# soma 1 ao n° de elementos e printa
-	la $a0, numElemMsg
-	syscall
-	li $v0, 1
-	addi $s1, $s1, 1		# O número de elementos é o número de espaços + 1
-	la $a0, ($s1)
-	syscall
+############## CONTA ELEMENTOS, OS CONVERTE PARA INTEIRO E GUARDA EM ARRAY ##############
+UsarBuffer:
+	la 	$s0, buffer	
+	la 	$s1, numbers             	# Guarda números em array
 	
-	j Fim
+LerBuffer:  
+	lbu 	$t1, 0($s0)  			# carrega um byte do buffer
+	beqz 	$t1, numElem  			# if $t1 == 0, ou seja, null terminator, então finaliza indo para numElem
 	
-LerBuffer:
-	la 	$s0, buffer
-	li 	$s1, 0      		# contador para elementos
+	beq 	$t1, 32, SpaceFound		# desvia se encontrar espaço (ascii = 32)
 	
-	la 	$s3, elem               # guarda números a serem concatenados (grandes)
-	
-pulaEspaco:  
-	lb $t1, 0($s0)  		# Load the first byte from address in $t1 
-	beqz $t1, PrintaElementos  	# if $t1 == 0 then go to label end
-	
-	beq $t1, 32, contaElem		# branch if symbol equals 32 (" ")
-	
-	# concatena número
-	# ---
-	
-	li $v0, 4
-	la $a0, 0($s0)
-	syscall
-	la $a0, newline
-	syscall
-	
-	addi $s0, $s0, 1      		# increment the address -- vai pra prox numero
-	j pulaEspaco
-
-contaElem:
-	#jal converteStrToInt           # valor estará salvo em $s3, é feito quando um espaço é achado - o número acabou
-	#la 	$s3, elem               # apaga o que tinha em elem para guardar novo número
-
-	addi $s0, $s0, 1      		# increment the address -- pula espaço
-	addi $s1, $s1, 1		# O número de elementos é o número de espaços + 1
-	
-    	j pulaEspaco 
+	# Converte ascii para int
+	addi    $t1, $t1, -48    
+    	mul    $t4, $t4, 10            		# Multiply word by 10
+    	add    $t4, $t4, $t1       		# numero += array[s1]-'0'
     	
-############### CONVERTE STRING PARA INTEIRO E SALVA EM ARRAY ################   
-converteStrToInt:
-	 			
+    	# Salva int em array
+    	sll $t0, $t9, 2           		# acerta i - só incrementa em spacefound
+	add $t0, $t0, $s1         		# acerta numbers[i]
+	sw $t4, 0($t0)
     	
+	addi 	$s0, $s0, 1      		# incrementa endereço do buffer (vai pra prox numero)
+	j LerBuffer
+
+SpaceFound:
+	add 	$t4, $zero, $zero		# zera variavel da conversao str -> int
+	addi 	$t9, $t9, 1			# incrementa i para novo número em array
+	addi 	$s0, $s0, 1      		# incrementa endereço do buffer (pula espaço)
+	addi 	$s2, $s2, 1			# O número de elementos é o número de espaços + 1 (somado posteriormente)
+	
+    	j LerBuffer
+
+numElem:
+	addi 	$s2, $s2, 1			# O número de elementos é o número de espaços + 1
+	
+##################### PROGRAMA PRINCIPAL #######################
+
+# int tam (Tamanho do vetor a ser ordenado): $s2
+# int *vetor (Vetor a ser ordenado): $s1 - endereço base
+main:
+
+ordena:
+	
 ############### FECHA ARQUIVO E TERMINA PROGRAMA ###############
 Fim:
 	# FECHAR ARQUIVO
-	li   $v0, 16       # system call for close file
-	move $a0, $s6      # file descriptor to close
-	syscall            # close file
+	li   	$v0, 16       			# system call for close file
+	move 	$a0, $s6      			# file descriptor to close
+	syscall            			# close file
 	
 	# TERMINA EXECUÇÃO
-	li $v0, 10         # termina programa
+	li 	$v0, 10         		# termina programa
 	syscall
-	
-	
 	
