f1ddbe321d57200e249d25b330a9581bd17eadf8
Camila Bezerril <camilabezerril@outlook.com>
2020-10-11 22:00:50 -0300

    add print resultado - sem escrever em file

--- Arquitetura de Computadores/Ordenacao.asm
+++ Arquitetura de Computadores/Ordenacao.asm
@@ -1,35 +1,43 @@
 .data
-file: .asciiz "teste.txt"      # filename for input
+in: .asciiz "in.txt"      # filename for input
+out: .asciiz "out.txt"
 buffer: .space 512             # 512 / 4 = 128 space for words
 
 .align 4
 numbers: .space 512
 
-ordenados: .space 512          # Armazena ascii para colocar em txt
+temp: .space 512
+buffer_ordenados: .space 512          # Armazena ascii para colocar em txt
 
 # Mensagens
 newline: .asciiz "\n"
+space: .asciiz " "
 
 .text
 # --------------------------------- LEITURA DO ARQUIVO -------------------------------------- #
-abrirArquivo:
+abrirIn:
 	li	$v0, 13          		# system call for open file
-	la	$a0, file        		# input file name
+	la	$a0, in        			# input file name
 	li	$a1, 0           		# flag for read-only
 	li	$a2, 0           		# mode is ignored
 	syscall			 		# open a file 
 	move	$s0, $v0         		# save the file descriptor 
 	
-lerArquivo:
+lerIn:
 	li	$v0, 14				# 14 = read from file
 	move 	$a0, $s0      			# file descriptor 
 	la	$a1, buffer			# buffer to hold int charged in a1
 	li	$a2, 512			# Read 512 bytes - size of buffer
 	syscall
+
+fecharIn:
+	li   	$v0, 16       			# system call for close file
+	move 	$a0, $s6      			# file descriptor to close
+	syscall            			# close file
 	
 # -------------- CONTA ELEMENTOS, OS CONVERTE PARA INTEIRO E ARMAZENA EM ARRAY ---------------- #
 usarBuffer:
-	la 	$s0, buffer	
+	la 	$s0, buffer
 	la 	$s1, numbers             	# Guarda números em array
 	
 lerBuffer:  
@@ -74,24 +82,19 @@ main:
 	la 	$a0, ($s1)
 	la 	$a1, ($s2)
 	la 	$a2, ($s3)
+	li	$a3, 0                          # Usado no quickSort para o index_menor
 	
 	beq 	$a2, $zero, selectionSort
 	beq 	$a2, 1, quickSort
 	
-	add 	$s4, $s4, $v0		   	# tamanho do vetor resultante retorna em $v0
+	add 	$s1, $zero, $v0		   	# tamanho do vetor resultante retorna em $v0
 	
-	li $v0, 1
-	lw $t0, 0($a0)
-	la $a0, ($t0)
-	syscall
-	
-	j Fim
+	j usarBuffers 	#INDA N FUNCIONA
 
 # -------------------------------- SELECTION SORT --------------------------------- #
 
 # public static void selectionSort(int[] arr){  
-#        for (int i = 0; i < arr.length - 1; i++)  
-#        {  
+#        for (int i = 0; i < arr.length - 1; i++){  
 #            int index = i;  
 #            for (int j = i + 1; j < arr.length; j++){  
 #                if (arr[j] < arr[index]){  
@@ -146,25 +149,139 @@ troca:
    	sw 	$t4, 0($t3)
    	sw 	$t5, 0($t2)
    	
-atualizaForFora:
+incrementaForFora:
 	addi	$s0, $s0, 1                     # incrementa i
 	j 	forFora
 
 fimSelection:
 	add 	$v0, $v0, $s1                   # retorna j, isto é, qtd de elementos no vetor resultante
-
-# ----------------------------------- QUICK SORT ---------------------------------- #
-quickSort:
-
 	
-# ------------------------ FECHA ARQUIVO E TERMINA PROGRAMA ----------------------- #
-Fim:	
-	# FECHAR ARQUIVO
+	la 	$s0, numbers
+	la	$s0, ($a0)			# salva array ordenado em $s0
+
+	j usarBuffers 	# TEMPORARIO
+
+# ----------------------------------- QUICK SORT - INCOMPLETO AINDA ---------------------------------- #
+
+#quickSort(arr[], low, high)
+#{
+#    if (low < high){
+#        // pivot_index is partitioning index, arr[pivot_index] is now at correct place in sorted array
+#        pivot_index = partition(arr, low, high);
+#
+#        quickSort(arr, low, pivot_index - 1);  // Before pivot_index
+#        quickSort(arr, pivot_index + 1, high); // After pivot_index
+#    }
+#}
+
+#partition (arr[], low, high){
+#    // pivot - Element at right most position
+#    pivot = arr[high];  
+#    i = (low - 1);  // Index of smaller element
+#    for (j = low; j <= high-1; j++){
+#        // If current element is smaller than the pivot, swap the element with pivot
+#        if (arr[j] < pivot){
+#            i++;    // increment index of smaller element
+#            swap(arr[i], arr[j]);
+#        }
+#    }
+#    swap(arr[i + 1], arr[high]);
+#    return (i + 1);
+#}
+
+# pivot = $s0
+
+quickSort:	
+	subi $sp, $sp, -12		
+	sw $a0, 12($sp)			# salva array atual
+	sw $a1, 8($sp)			# salva index_maior atual
+	sw $a3, 4($sp)			# salva index_menor atual
+	sw $ra, 0($sp)		
+	move $fp, $sp
+	
+	slt $t1, $a1, $a1		# t1=1 if low < high, else 0
+	beq $t1, $zero, fimQuick	# if low >= high, quick
+	
+	jal particionar
+	add $s0, $v0, $v0
+	
+
+particionar:
+
+fimQuick:
+	
+
+# ------------------ CONVERTE ARRAY INT -> STR, ESCREVE EM TXT E FINALIZA PROGRAMA ------------------- #
+
+# $s0 = array dos numeros ordenados a serem escritos
+# $s2 = tamanho do vetor
+
+usarBuffers:
+	#la 	$s1, buffer_ordenados
+	li 	$t0, 0
+	li	$t1, 0
+	li	$t2, 0
+	li	$t3, 0
+	li 	$t4, 0
+	
+escreverBuffers:
+	beq	$t0, $s2, abrirOut		# i = número de elementos do vetor? se sim, escrever buffer no arquivo
+	
+	sll 	$t1, $t0, 2			# acerta i
+   	add 	$t1, $t1, $s0			# $s0[i]   	
+   	lw 	$t1, 0($t1)
+   	
+   	itoa:
+   		la   $s3, temp + 30
+      		#add  $s3, $s3, 30   # seek the end
+      		sb   $0, 1($s3)     # null-terminated str
+      		li   $t2, '0'  
+      		sb   $t2, ($s3)     # init. with ascii 0      
+      		li   $t3, 10        # preload 10
+      		beq  $t1, $0, fimItoa  # end if 0
+      
+	loop:
+      		div  $t1, $t3       # a /= 10
+      		mflo $t1
+      		mfhi $t4            # get remainder
+      		add  $t4, $t4, $t2  # convert to ASCII digit
+      		sb   $t4, ($s3)     # store it
+      		sub  $s3, $s3, 1    # decrease buffer pointer
+      		bne  $t1, $0, loop  # if not zero, loop
+      		addi $s3, $s3, 1    # adjust buffer pointer
+      
+	fimItoa:
+		li $v0, 4
+   		la $a0, ($s3)
+   		syscall
+   		la $a0, newline
+   		syscall
+   	
+   	
+   	addi	$t0, $t0, 1
+   	j escreverBuffers 			
+
+abrirOut:
+	li	$v0, 13          		# system call for open file
+	la	$a0, out        		# input file name
+	li	$a1, 1           		# flag for write-only
+	li	$a2, 0           		# mode is ignored
+	syscall			 		# open a file 
+	move	$s0, $v0         		# save the file descriptor 
+	
+escreverOut:
+	li	$v0, 15				# 15 = write from file
+	move 	$a0, $s0      			# file descriptor 
+	la	$a1, ($s3)			# buffer to hold int charged in a1
+	li	$a2, 512			# Write 512 bytes - size of buffer
+	syscall
+	
+fecharOut:
 	li   	$v0, 16       			# system call for close file
 	move 	$a0, $s6      			# file descriptor to close
 	syscall            			# close file
 	
-	# TERMINA EXECUÇÃO
+fim:	
 	li 	$v0, 10         		# termina programa
 	syscall
 	
