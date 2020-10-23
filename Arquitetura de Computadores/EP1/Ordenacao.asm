9b365d3453faa9af73daccb0956400b8883f25d2
Camila Bezerril <camilabezerril@outlook.com>
2020-10-23 18:06:05 -0300

    entrada pelo usuario

--- Arquitetura de Computadores/EP1/Ordenacao.asm
+++ Arquitetura de Computadores/EP1/Ordenacao.asm
@@ -1,6 +1,7 @@
 .data
 # Arquivos
-in: .asciiz "in.txt"      		# Entrada
+in: .space 80    			# Entrada
+ord: .space 4				# Define ordenação
 
 # Buffers
 buffer: .space 512             		# Armazena ascii de números (Entrada)
@@ -9,9 +10,11 @@ buffer_ordenados: .space 512		# Armazena números ascii para colocar em txt (Saíd
 
 # Vetor
 .align 4
-numbers: .space 512			# Armazena números em vetor
+numeros: .space 512			# Armazena números em vetor
 
 # Mensagens
+entrada: .asciiz "Qual é o nome do arquivo de entrada? "
+ordenacao: .asciiz "Qual é a ordenação a ser utilizada (0 = selection, 1 = quick)? "
 newline: .asciiz "\n"
 
 .text
@@ -24,7 +27,11 @@ main:
 	# tamanho do vetor resultante da ordenação: $s3
 	# quantidade de caracteres lidos na entrada: $s4
 	# buffer ascii com números ordenados: $s5
+	# ordenação a ser utilizada: $s6
 
+	# Lê entradas do usuário
+	jal entradaUsuario
+	
 	# Leitura de Arquivo de Entrada
 	jal iniciarLeitura
 	
@@ -32,9 +39,9 @@ main:
 	jal iniciarBuffer_In
 	
 	la 	$a0, ($s1)
-	la 	$a1, ($s2)
-	la 	$a2, 0				# Define menor index do vetor (Usado no quickSort)
-	li	$a3, 0                          # Define tipo ordenação (0 = selection, 1 = quick)
+	li 	$a1, 0				 # Define menor index do vetor (Usado no quickSort)
+	la 	$a2, ($s2)				
+	la	$a3, ($s6)                       # Define tipo ordenação (0 = selection, 1 = quick)
 	
 	# Ordenar números
 	jal ordena
@@ -46,6 +53,50 @@ main:
 	
 	# Escrita de Arquivo de Saída e finaliza programa
 	j iniciarEscrita
+
+# -------------------------------- ENTRADA DO USUÁRIO --------------------------------------- #
+entradaUsuario:
+	subi $sp, $sp, 12
+	sw $t2, 8($sp)
+	sw $t1, 4($sp)
+	sw $t0, 0($sp) 
+
+	# Qual é o nome do arquivo de entrada?
+	li	$v0, 4
+	la	$a0, entrada
+	syscall
+	li	$v0, 8
+	la 	$a0, in
+	li 	$a1, 80
+	syscall
+	
+	la	$t2, in
+	
+	corrigirIn:  
+		lb 	$t0, 0($t2)  			# carrega um byte do buffer
+		beq 	$t0, 10, foundChar		
+	
+		addi 	$t2, $t2, 1      		# incrementa endereço do buffer (vai pra prox numero)
+		j 	corrigirIn
+
+	foundChar:
+		addi	$t1, $zero, 0
+		sb	$t1, 0($t2)			# substitui char 10 por 0
+	
+DefineOrdenacao:	
+	li	$v0, 4
+	la	$a0, ordenacao
+	syscall
+	li 	$v0, 5
+	syscall
+	move 	$s6, $v0
+	
+	lw $t0, 0($sp)
+	lw $t1, 4($sp)
+	lw $t2, 8($sp)
+	addi $sp, $sp, 12
+	
+	jr $ra
 
 # --------------------------------- LEITURA DO ARQUIVO -------------------------------------- #
 iniciarLeitura:
@@ -95,7 +146,7 @@ iniciarBuffer_In:
 
 usarBuffer:
 	la 	$t4, buffer
-	la 	$s1, numbers             	# Guarda números em array
+	la 	$s1, numeros             	# Guarda números em array
 	
 lerBuffer:  
 	lbu 	$t1, 0($t4)  			# carrega um byte do buffer
@@ -108,9 +159,9 @@ lerBuffer:
     	mul    	$t3, $t3, 10            	# multiplica palavra por 10
     	add    	$t3, $t3, $t1       		# numero += array[s1]-'0'
     	
-    	# Salva int em array
+    	# Salva int em vetor
     	sll 	$t0, $s2, 2           		# acerta i - só incrementa em spacefound
-	add 	$t0, $t0, $s1         		# acerta numbers[i]
+	add 	$t0, $t0, $s1         		# acerta numeros[i]
 	sw 	$t3, 0($t0)
     	
     	addi	$s4, $s4, 1			# conta numero de caracteres entrando (usado na saida depois)
@@ -142,27 +193,13 @@ finalizarBuffer_In:
 # --------------------------------- INICIA ORDENAÇÃO -------------------------------------- #	
 ordena:
 	beq 	$a3, $zero, iniciarSelection
-	beq 	$a3, 1, iniciarQuick	
+	beq 	$a3, 1, quickSort	
 
 # ------------------------------------ SELECTION SORT ------------------------------------- #
 
-# public static void selectionSort(int[] arr){  
-#        for (int i = 0; i < arr.length - 1; i++){  
-#            int index = i;  
-#            for (int j = i + 1; j < arr.length; j++){  
-#                if (arr[j] < arr[index]){  
-#                    index = j;//searching for lowest index  
-#                }  
-#            }  
-#            int smallerNumber = arr[index];   
-#            arr[index] = arr[i];  
-#            arr[i] = smallerNumber;  
-#        }  
-#    }  
-
 # $t6 = i, $t7 = j, $t1 = index
-# $a1 = array.length, $a0 = arr
-# resultado está em $a0 num array
+# $a2 = numeros.length, $a0 = numeros
+# resultado está em $a0 num vetor
 
 iniciarSelection:
 	subi $sp, $sp, 32
@@ -176,25 +213,24 @@ iniciarSelection:
 	sw $t0, 0($sp)
 
 selectionSort:
-	la 	$t6, 0                          # Inicio do array -> $t0 = min
-	addi 	$t0, $a1, -1			# $t1 = array.length - 1
+	addi 	$t0, $a2, -1			# $t1 = numeros.length - 1
 	
 forFora:
-	beq 	$t6, $t0, fimSelection      	# Se i = array.length - 1 -> desvio
+	beq 	$t6, $t0, fimSelection      	# Se i = numeros.length - 1 -> desvio
 	add 	$t1, $t6, $zero             	# index = $s0 (i)
 	
 	addi 	$t7, $t6, 1                	# j = i + 1 -> inicio do for de dentro
 	
 forDentro:
-	beq 	$t7, $a1, troca			# Se j = array.length -> desvio
+	beq 	$t7, $a2, troca			# Se j = numeros.length -> desvio
 	sll 	$t2, $t1, 2                     # acerta index
-	add 	$t2, $t2, $a0                   # acerta numbers[index]
-	lw 	$t2, 0($t2)               	# $t2 = numbers[index]
+	add 	$t2, $t2, $a0                   # acerta numeros[index]
+	lw 	$t2, 0($t2)               	# $t2 = numeros[index]
    	sll 	$t3, $t7, 2                     # acerta j
-   	add 	$t3, $t3, $a0			# acerta numbers[j]
-   	lw 	$t3, 0($t3)                  	# $t3 = numbers[j]
-   	slt	$t2, $t3, $t2			# numbers[j] < numbers[index]?
-   	bne 	$t2, $zero, atualizaIndex     	# Se sim ($t2 = 1), troca numbers[index] por numbers[j]
+   	add 	$t3, $t3, $a0			# acerta numeros[j]
+   	lw 	$t3, 0($t3)                  	# $t3 = numeros[j]
+   	slt	$t2, $t3, $t2			# numeros[j] < numeros[index]?
+   	bne 	$t2, $zero, atualizaIndex     	# Se sim ($t2 = 1), troca numeros[index] por numeros[j]
    	
    	add 	$t7, $t7, 1 			# incrementa j
    	j 	forDentro
@@ -206,9 +242,9 @@ atualizaIndex:
    	
 troca:	
    	sll 	$t2, $t6, 2                     # acerta i
-   	add 	$t2, $t2,$a0			# numbers[i]
+   	add 	$t2, $t2,$a0			# numeros[i]
    	sll 	$t3, $t1, 2			# acerta index   
-   	add 	$t3, $t3,$a0			# numbers[index]
+   	add 	$t3, $t3,$a0			# numeros[index]
    	lw 	$t4, 0($t2)        
    	lw 	$t5, 0($t3)        
    	sw 	$t4, 0($t3)
@@ -221,7 +257,7 @@ incrementaForFora:
 fimSelection:
 	addi	$v0, $t6, 1			# retorna i + 1, isto é, qtd de elementos no vetor resultante               
 	
-	la 	$s0, numbers
+	la 	$s0, numeros
 	la	$s0, ($a0)			# salva array ordenado em $s0
 	
 	lw 	$t0, 0($sp)
@@ -238,55 +274,127 @@ fimSelection:
 	
 # ----------------------------------- QUICK SORT - INCOMPLETO AINDA ---------------------------------- #
 
-#quickSort(arr[], low, high)
-#{
-#    if (low < high){
-#        // pivot_index is partitioning index, arr[pivot_index] is now at correct place in sorted array
-#        pivot_index = partition(arr, low, high);
-#
-#        quickSort(arr, low, pivot_index - 1);  // Before pivot_index
-#        quickSort(arr, pivot_index + 1, high); // After pivot_index
-#    }
-#}
-
-#partition (arr[], low, high){
-#    // pivot - Element at right most position
-#    pivot = arr[high];  
-#    i = (low - 1);  // Index of smaller element
-#    for (j = low; j <= high-1; j++){
-#        // If current element is smaller than the pivot, swap the element with pivot
-#        if (arr[j] < pivot){
-#            i++;    // increment index of smaller element
-#            swap(arr[i], arr[j]);
-#        }
-#    }
-#    swap(arr[i + 1], arr[high]);
-#    return (i + 1);
-#}
-
-# pivot = $t0
-
-iniciarQuick:
-
 quickSort:	
-	subi $sp, $sp, -20
-	sw $ra, 16($sp)
-	sw $fp, 12($sp)	
-	sw $a0, 8($sp)			
-	sw $a1, 4($sp)			
-	sw $a2, 0($sp)					
-	move $fp, $sp
+	subi 	$sp, $sp, 20
+	sw 	$ra, 16($sp)
+	sw 	$fp, 12($sp)	
+	sw 	$a0, 8($sp)			# Numeros (vetor)		
+	sw 	$a1, 4($sp)			# Inicio			
+	sw 	$a2, 0($sp)			# Fim			
+	move 	$fp, $sp
 	
-	slt $t1, $a1, $a1		# t1=1 if low < high, else 0
-	beq $t1, $zero, fimQuick	# if low >= high, fimQuick -> retorna todo a pilha da recursão?
+	slt 	$t1, $a1, $a2			# se inicio < fim = 1
+	beq 	$t1, $zero, fimQuick		# se inicio >= fim, fimQuick
 	
-	jal particionar
-	add $s0, $v0, $v0
+	jal 	particionar
+	add 	$t0, $v0, $zero			# pivot_index = particionar
 	
+	add	$t2, $a2, $zero			# Salva fim antes da recursão
+	subi	$a2, $t2, 1			# novo fim = pivot - 1
+	jal 	quickSort
+	
+	add	$a2, $t2, $zero			# Restaura fim
+	subi	$a1, $t0, 1			# novo inicio = pivot + 1
+	jal	quickSort
 
 particionar:
+	subi 	$sp, $sp, 36
+	sw 	$ra, 32($sp)			# particionar chama troca, portanto salvar ra
+	sw 	$fp, 28($sp)
+	sw	$a3, 24($sp)		
+	sw	$a1, 20($sp)			# arg são reutilizados em troca
+	sw	$t0, 16($sp)
+	sw	$t1, 12($sp)
+	sw	$t2, 8($sp)
+	sw	$t3, 4($sp)
+	sw	$t4, 0($sp)			
+	move 	$fp, $sp
+	
+	sll	$t0, $a2, 2			
+	add	$t0, $a0, $t0			# numeros[fim]
+	lw	$t0, 0($t0)			# pivot = numeros[fim]
+	
+	subi	$t1, $a1, 1			# i = inicio - 1
+	add	$t2, $a1, $zero			# j = inicio
+	
+	for:
+		beq 	$t2, $a2, fimFor		# j = fim?
+		
+		sll	$t4, $t2, 2			
+		add	$t4, $a0, $t4			# numeros[j]
+		lw	$t4, 0($t4)			# valor de numeros[j]
+		
+		slt	$t4, $t4, $t0			# numeros[j] < pivot?
+		beq	$t4, $zero, maior		# Se numeros[j] > pivot -> maior
+		
+		addi 	$t1, $t1, 1			# i++
+		
+		add	$a1, $t1, $zero			# arg1 = i --> para função troca
+		add	$a3, $t2, $zero			# arg3 = j --> para função troca
+		jal 	trocaQuick
+		
+		maior:
+		add	$t2, $t2, 1			# j++
+		
+		j	for
+	
+fimFor:
+	add 	$a1, $t1, 1				# arg2 = i + 1
+	add	$a3, $a2, $zero				# arg3 = fim
+	jal 	trocaQuick
+	
+	addi 	$v0, $t1, 1				# retorna i + 1
+	
+	lw 	$t4, 0($sp)
+	lw 	$t3, 4($sp)
+	lw 	$t2, 8($sp)
+	lw 	$t1, 12($sp)
+	lw 	$t0, 16($sp)
+	lw 	$a1, 20($sp)
+	lw 	$a3, 24($sp)
+	lw 	$fp, 28($sp)
+	lw 	$ra, 32($sp)
+	addi 	$sp, $sp, 36
+	
+	jr $ra
+	
+trocaQuick:
+	subi 	$sp, $sp, 16
+	sw	$t0, 12($sp)			
+	sw	$t1, 8($sp)
+	sw	$t2, 4($sp)
+	sw	$t3, 0($sp)		
 
+	sll	$t1, $a1, 2			# arg1 * 4
+	add	$t1, $a0, $t1			# numeros[arg1]
+	
+	sll	$t0, $a3, 2			# arg3 * 4
+	add	$t0, $a0, $t0			# numeros[arg3]
+	
+	lw	$t2, 0($t1)			# valor de numeros[arg1]
+	lw	$t3, 0($t0)			# valor de numeros[arg3]
+	sw	$t2, 0($t0)			
+	sw	$t3, 0($t1)
+				
+	lw $t3, 0($sp)
+	lw $t2, 4($sp)
+	lw $t1, 8($sp)
+	lw $t0, 12($sp)
+	addi $sp, $sp, 16
+	
+	jr $ra
+	
 fimQuick:
+	la 	$s0, numeros
+	la	$s0, ($a0)			# salva array ordenado em $s0
+
+	lw 	$a2, 0($sp)
+	lw 	$a1, 4($sp)
+	lw 	$a0, 8($sp)
+	lw 	$fp, 12($sp)
+	lw 	$ra, 16($sp)
+	addi 	$sp, $sp, 20
+	
 	jr $ra
 
 # ---------------------------- CONVERTE VETOR ORDENADO INT -> STR -------------------------- #
